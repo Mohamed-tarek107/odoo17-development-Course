78faf833b843abdd32fa61cd2c66a3ecd1b9976b
Mohamed <mohamedtarekk710@gmail.com>
2026-04-27 08:31:14 +0300

    Added 2 menu views base and property_view added them to manifest wiht secuirty having access to data

--- custom_addons/app_one/__manifest__.py
+++ custom_addons/app_one/__manifest__.py
@@ -4,6 +4,10 @@
     'version': '17.0.0.1.0',
     'category': '',
     'depends': ['base'],
-    'data': [],
+    'data': [
+        'secuirty/ir.model.access.csv',
+        'views/base_menu.xml',
+        'views/property_view.xml'
+    ],
     'application': True
 }
