93ce2e5e19c3c6cf528ff4bc96267849e027e787
Mohamed <mohamedtarekk710@gmail.com>
2026-05-29 07:47:47 +0300

    Added input validation on logic using execptions raising and attributes inside fields

--- custom_addons/app_one/models/property.py
+++ custom_addons/app_one/models/property.py
@@ -1,11 +1,13 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+
 
 class Property(models.Model):
     _name = 'property'
 
-    name = fields.Char()
+    name = fields.Char(required=1, default='New', size=10)
     description = fields.Text()
-    postcode = fields.Char()
+    postcode = fields.Char(required=1)
     Date_availability = fields.Date()
     selling_price = fields.Float()
     bedrooms = fields.Integer()
@@ -20,3 +22,10 @@ class Property(models.Model):
     ('east', 'East'),
     ('west', 'West'),
 ])
+
+    @api.constraints('bedrooms')
+    def _check_bedroom_greater_zero(self):
+        for rec in self:
+            if rec.bedrooms == 0:
+                # print("not valid")
+                raise ValidationError('Add valid number of bedrooms')
