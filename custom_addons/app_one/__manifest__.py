8df731c2d3de6ab5a3555c5200df6d2c17fa667d
Mohamed <mohamedtarekk710@gmail.com>
2026-06-09 12:27:44 +0300

    Added styling to views using css

--- custom_addons/app_one/__manifest__.py
+++ custom_addons/app_one/__manifest__.py
@@ -11,5 +11,8 @@
         'views/owner_view.xml',
         'views/tag_view.xml'
     ],
+    'assets': {
+        'web.assets_backend': ['app_one\static\src\css\property.css']
+    },
     'application': True
 } 
